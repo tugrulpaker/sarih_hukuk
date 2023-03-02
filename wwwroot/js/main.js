9baea3685a4869926a958db41628a346fdd845a2
tugrulpaker <tugrulpaker@hotmail.com>
2023-03-03 02:29:08 +0300

    mete.index and calismaalanlari

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -25,6 +25,34 @@
             $('.carousel, .page-header').css("margin-top", "0");
         }
     });
+
+    // Service carousel
+    $(".service-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
     
     
     // Dropdown on mouse hover
